353629de03e4a994b98515da8d4c1db6430219d1
KieuOanhUIT <22521062@gm.uit.edu.vn>
2025-07-03 12:21:37 +0700

    Landing screen

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,24 @@
-import { Image, Text, View } from "react-native";
+import { UserDetailContext } from "@/context/UserDetailContext";
+import { useRouter } from "expo-router";
+import { onAuthStateChanged } from 'firebase/auth';
+import { doc, getDoc } from 'firebase/firestore';
+import { useContext } from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { auth, db } from './../config/firebaseConfig';
 import Colors from './../constants/Colors';
-
 export default function Index() {
+
+    const router = useRouter();
+    const [userDetail, setUserDetail] = useContext(UserDetailContext)
+    onAuthStateChanged(auth,async(user)=>{
+        if(user){
+            console.log(user);
+            const result=await getDoc(doc(db,'users',user?.email));
+            setUserDetail(result.data())
+            router.replace('/(tabs)/home')
+        }
+    })
+
     return (
         <View
             style={{
@@ -36,17 +53,36 @@ export default function Index() {
                 </Text>
                 <Text style={{
                     fontSize: 16, color: 'white', marginTop: 20, textAlign:'center'
-                }}>Transform your ideas into engaging educational content, effortlessly with AI!</Text>
-                <View>
-                    <Text>Get started</Text>
-                </View>
+                }}>
+                    Transform your ideas into engaging educational content, effortlessly with AI!
+                </Text>
+                <TouchableOpacity style={styles.button}
+                    onPress={() => router.push('/auth/signUp')}
+                >
+                    <Text style={[styles.buttonText, {color: Colors.PRIMARY }]}>Get started</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={[styles.button, {
+                    backgroundColor: Colors.PRIMARY, borderWidth: 2, borderColor: 'white'
+                }]}
+                onPress={()=>router.push('/auth/signIn')}
+                >
+                    <Text style={[styles.buttonText, {color: 'white'}]}>Already have an account?</Text>
+                </TouchableOpacity>
             </View>
         </View>
     );
 }
 
-const styles = StyleSheet.name({
+const styles = StyleSheet.create({
     button:{
-        padding: 15, backgroundColor: Colors.PRIMARY
+        padding: 15,
+        backgroundColor: 'white',
+        marginTop: 20,
+        borderRadius: 10,
+        width: '80%'
+    },
+    buttonText:{
+        textAlign: 'center',
+        fontSize: 18,
     }
-})
+});
