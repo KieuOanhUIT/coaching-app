454f76115b8694174babe429421fbb19126f1b8c
KieuOanhUIT <22521062@gm.uit.edu.vn>
2025-07-03 12:22:50 +0700

    update

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
 import 'react-native-reanimated';
+import { UserDetailContext } from './../context/UserDetailContext';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useState } from 'react';
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
@@ -12,18 +12,32 @@ export default function RootLayout() {
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   });
 
+const [userDetail, setUserDetail] = useState({});
+
+  // Su dung font chu khac
+  // const [loaded, error] = useFonts({
+  //   'outfit': require('./../assets/fonts/<ten file ttf'),
+  //   'outfit-bold': require('./../assets/fonts/<ten file tff bold')
+  // })
+
   if (!loaded) {
     // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    // <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    //   <Stack>
+    //     <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+    //     <Stack.Screen name="+not-found" />
+    //   </Stack>
+    //   <StatusBar style="auto" />
+    // </ThemeProvider>
+    <UserDetailContext.Provider value={[userDetail, setUserDetail]}>
+      <Stack screenOptions={{
+        headerShown: false
+      }}>
       </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    </UserDetailContext.Provider>
   );
 }
--- constants/Colors.ts
+++ constants/Colors.ts
@@ -7,8 +7,15 @@ const tintColorLight = '#0a7ea4';
 const tintColorDark = '#fff';
 
 export default{
-  WHITE: 'fff',
-  PRIMARY: '#0075ff'
+  WHITE: '#fff',
+  PRIMARY: '#0075ff',
+  GRAY: '#858585',
+  BG_GRAY: '#f2f2f2',
+  BLACK: '#000',
+  GREEN: '#24b351',
+  LIGHT_GREEN: '#dbffdd',
+  RED: '#f51c0c',
+  LIGHT_RED: '#ffc8c4'
 }
 
 export const Colors = {
