3c98609dbf288a1e225d064193fd2f449b8ea507
KieuOanhUIT <22521062@gm.uit.edu.vn>
2025-07-08 11:37:38 +0700

    chapter screen and logic

---
+++ app/chapterView/index.jsx
@@ -0,0 +1,81 @@
+import { useLocalSearchParams, useRouter } from 'expo-router';
+import { arrayUnion, doc, updateDoc } from 'firebase/firestore';
+import { useState } from 'react';
+import { Dimensions, StyleSheet, Text, View } from 'react-native';
+import * as Progress from 'react-native-progress';
+import Button from '../../components/Shared/Button';
+import { db } from '../../config/firebaseConfig';
+import Colors from '../../constants/Colors';
+
+export default function ChapterView() {
+    const { chapterParams, docId, chapterIndex, courseParams } = useLocalSearchParams();
+    const chapters = JSON.parse(chapterParams);
+    const [currentPage, setCurrentPage] = useState(0);
+    const [loader, setLoader] = useState(false);
+    const router = useRouter();
+    const GetProgress = (currentPage) => {
+        const perc = (currentPage / chapters?.content.length);
+        return perc;
+    }
+    const onChapterComplete = async () => {
+        // Luu chapter complete
+        setLoader(true)
+        await updateDoc(doc(db, 'Courses', docId), {
+            completedChapter: arrayUnion(chapterIndex)
+        })
+        setLoader(false)
+        // router.replace({
+        //     pathname: '/courseView',
+        //     params: {
+        //         courseParams: courseParams
+        //     }
+        // });
+        router.back();
+
+        // Back
+    }
+    return (
+        <View style={{
+            padding: 25,
+            backgroundColor: 'white',
+            flex: 1
+        }}>
+            <Progress.Bar progress={GetProgress(currentPage)}
+                width={Dimensions.get('screen').width * 0.85} />
+            <View style={{
+                marginTop: 20
+            }}>
+                <Text style={{
+                    fontWeight: 'bold',
+                    fontSize: 25
+                }}>{chapters?.content[currentPage]?.topic}</Text>
+                <Text style={{
+                    fontSize: 20,
+                    marginTop: 7
+                }}>{chapters?.content[currentPage]?.explain}</Text>
+                {chapters?.content[currentPage]?.code && <Text style={[styles.codeExampleText, { backgroundColor: Colors.BLACK, color: 'white' }]}>{chapters?.content[currentPage]?.code}</Text>}
+                {chapters?.content[currentPage]?.example && <Text style={styles.codeExampleText}>{chapters?.content[currentPage]?.example}</Text>}
+            </View>
+            <View style={{
+                position: 'absolute',
+                bottom: 10,
+                width: '100%',
+
+            }}>
+                {chapters?.content?.length - 1 != currentPage ?
+                    <Button text={'Next'} onPress={() => setCurrentPage(currentPage + 1)}></Button>
+                    : <Button text={'Finish'} onPress={() => onChapterComplete()} loading={loader}></Button>}
+            </View>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    codeExampleText: {
+        padding: 15,
+        backgroundColor: Colors.BG_GRAY,
+        borderRadius: 15,
+        fontSize: 18,
+        marginTop: 10
+    }
+})
--- components/CourseView/Chapters.jsx
+++ components/CourseView/Chapters.jsx
@@ -1,19 +1,36 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { useRouter } from 'expo-router';
+import { FlatList, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import Colors from '../../constants/Colors';
-export default function Chapters({course}) {
+export default function Chapters({ course }) {
+  const router = useRouter();
+  const isChapterCompleted = (index) => {
+    const isCompleted = course?.completedChapter.find(item => item == index)
+    return isCompleted ? true : false
+  }
   return (
     <View style={{
-        padding: 20
-      }}>
+      padding: 20
+    }}>
       <Text style={{
         fontSize: 25,
         fontWeight: 'bold'
       }}>Chapters</Text>
       <FlatList
-      data={course?.chapters}
-      renderItem={({item, index})=>(
-        <View style={{
+        data={course?.chapters}
+        renderItem={({ item, index }) => (
+          <TouchableOpacity onPress={() => {
+            router.push({
+              pathname: '/chapterView',
+              params: {
+                chapterParams: item ? JSON.stringify(item) : {},
+                docId: course?.docId,
+                chapterIndex: index,
+                courseParams: JSON.stringify(course)
+              }
+            })
+          }} style={{
             padding: 15,
             borderWidth: 0.5,
             borderRadius: 15,
@@ -21,24 +38,25 @@ export default function Chapters({course}) {
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
-        }}>
+          }}>
             {/* <View style={{
                 display: 'flex',
                 flexDirection: 'row',
                 gap: 10
             }}> */}
-                {/* <Text style={styles.chapterText}>{index+1}.</Text> */}
-                <Text numberOfLines={1} style={[styles.chapterText, {flex: 1}]}>{item.chapterName}</Text>
-                {/* </View> */}
-                <FontAwesome name="play" size={24} color={Colors.PRIMARY} />
-        </View>
-      )}/>
+            {/* <Text style={styles.chapterText}>{index+1}.</Text> */}
+            <Text numberOfLines={1} style={[styles.chapterText, { flex: 1 }]}>{item.chapterName}</Text>
+            {/* </View> */}
+            {isChapterCompleted(index) ? <Ionicons name="checkmark-circle" size={24} color={Colors.GREEN} /> : <FontAwesome name="play" size={24} color={Colors.PRIMARY} />
+            }
+          </TouchableOpacity>
+        )} />
     </View>
   )
 }
 
-const styles= StyleSheet.create({
-    chapterText:{
-        fontSize: 20,
-    }
+const styles = StyleSheet.create({
+  chapterText: {
+    fontSize: 20,
+  }
 })
