53a0279f9681a9b753f0f29cfea965c1f2acbe15
KieuOanhUIT <22521062@gm.uit.edu.vn>
2025-07-08 15:06:41 +0700

    replace structure to fix dynamic and use courseId instead

---
+++ app/courseView/[courseId]/index.jsx
@@ -0,0 +1,40 @@
+import { useLocalSearchParams } from 'expo-router';
+import { doc, getDoc } from 'firebase/firestore';
+import { useEffect, useState } from 'react';
+import { FlatList, View } from 'react-native';
+import Chapters from '../../../components/CourseView/Chapters';
+import Intro from '../../../components/CourseView/Intro';
+import { db } from './../../../config/firebaseConfig';
+export default function CourseView() {
+    const { courseParams, courseId } = useLocalSearchParams();
+    const [course, setCourse]=useState([]);
+    // const course = JSON.parse(courseParams);
+    // console.log(courseId);
+
+    useEffect(()=>{
+        if(!courseParams){
+            GetCourseById();
+        } else{
+            setCourse(JSON.parse(courseParams));
+        }
+    },[courseId])
+
+    const GetCourseById = async () => {
+        const docRef = await getDoc(doc(db, 'Courses', courseId));
+        const courseData = docRef.data();
+        setCourse(courseData)
+    }
+
+    return course && (
+        <FlatList
+            data={[]}
+            ListHeaderComponent={
+                <View style={{
+                    flex: 1,
+                    backgroundColor: 'white'
+                }}>
+                    <Intro course={course} />
+                    <Chapters course={course} />
+                </View>} />
+    )
+}
